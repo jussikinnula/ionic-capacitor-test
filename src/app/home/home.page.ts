c4e1606dc350305364a9c58223c481364b176f2d
Jussi Kinnula <jussi.kinnula@varian.com>
2021-04-22 19:09:05 +0300

    Add buildTime environment variable + change build dir to build/app

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  buildTime = environment.buildTime;
 
   constructor() {}
 
