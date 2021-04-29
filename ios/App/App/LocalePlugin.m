100cfa2f01972ccf177fd6b894eb0622f3704458
Jussi Kinnula <jussi.kinnula@varian.com>
2021-04-29 08:31:30 +0300

    Fix iOS LocalePlugin.m naming

--- ios/App/App/LocalePlugin.m
+++ ios/App/App/LocalePlugin.m
@@ -1,5 +1,5 @@
 #import <Capacitor/Capacitor.h>
 
-CAP_PLUGIN(LocalePlugin, "LocalePlugin",
+CAP_PLUGIN(LocalePlugin, "Locale",
            CAP_PLUGIN_METHOD(getCountryCode, CAPPluginReturnPromise);
 )
